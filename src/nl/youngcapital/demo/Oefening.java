2ffe5d4a0eae5a5aa96961c058e32ed457327ccf
Joeri Venekamp <jvenekamp@gmail.com>
2016-11-09 16:42:56 +0100

    Update

--- src/nl/youngcapital/demo/Oefening.java
+++ src/nl/youngcapital/demo/Oefening.java
@@ -1,15 +1,19 @@
 package nl.youngcapital.demo;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import org.apache.tomcat.jni.Time;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 
 /**
@@ -29,18 +33,40 @@ public class Oefening extends HttpServlet {
 	
 	
 	
+	
+	
+	
+	
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		
+		
 		response.getWriter().append(
-		"<!DOCTYPE html><html><head>"
-		+ "<title>Test servlet</title>"
-		+ "<head><body><h1>Dit is de testservlet!</h1><br>"
-		+ "De tijd is: " + myTimeString + "<br>"
-		+ "De pagina is " + ++counter + " keer bezocht."
-		+ "</body></html>");
+				"<!DOCTYPE html><html><head>"
+				+ "<title>Test servlet</title>"
+				+ "<head><body><h1>Dit is de testservlet!</h1><br>"
+				+ "De tijd is: " + myTimeString + "<br>"
+				+ "De pagina is " + ++counter + " keer bezocht.<br>"
+				+ "<ul>");
+				
+		Enumeration<String> headerNames = request.getHeaderNames();
+		String header = headerNames.nextElement();
+		
+		while (headerNames.hasMoreElements()){
+		
+			response.getWriter().append("<li>" + header + "</li>");
+			
+			response.getWriter().append("</body></html>");
+		
+		}
+		
+		
+	
+		
 		
 	}
 
