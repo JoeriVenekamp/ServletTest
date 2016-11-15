5dd4295e0bcab844a23d0ed6582f744c3695bf4e
Joeri Venekamp <jvenekamp@gmail.com>
2016-11-15 12:07:54 +0100

    Update

--- src/nl/youngcapital/demo/FirstDynamicWebProject.java
+++ src/nl/youngcapital/demo/FirstDynamicWebProject.java
@@ -19,7 +19,6 @@ public class FirstDynamicWebProject extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter()
 		
 		.append("<!DOCTYPE html><html><head>" +
